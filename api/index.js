b63e37e17168a31c187711e07ef54d92e530c11c
unknown123sp-gif <unknown123sp@gmail.com>
2026-04-15 20:19:10 +0530

    Use explicit Vercel static and API routes

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const { handleApi } = require("../server");
+const { handleApi } = require("../local-server");
 
 module.exports = async function handler(req, res) {
   const host = req.headers.host || "localhost";
